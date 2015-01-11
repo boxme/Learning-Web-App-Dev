458cd79fba5cb4a4b05dda9da40b1b13c737106b
boxme <desmond.ng.yang.yi@gmail.com>
2015-01-11 21:52:39 +0800

    Check for empty text before adding the text into the paragraph

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -2,14 +2,16 @@ var main = function() {
 	"use-strict";
 
 	$(".comment-input button").on("click", function(event) {
-		// Add a $ to the var name if it holds a jQuery object
-		// jQuery allows for chaining function calls
-		var $new_comment = $("<p>");
 		var comment_text = $(".comment-input input").val();
 
-		$new_comment.text(comment_text);
-
-		$(".comments").append($new_comment);
+		// Ensure that comment_text is not empty
+		if (comment_text !== "") {
+			// Add a $ to the var name if it holds a jQuery object
+			// jQuery allows for chaining function calls
+			var $new_comment = $("<p>");
+			$new_comment.text(comment_text);
+			$(".comments").append($new_comment);
+		}
 	});
 };
 
