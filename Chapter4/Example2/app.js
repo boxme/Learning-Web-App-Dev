1387ce3f13a5611344dda67486f8f2dfa36802cb
boxme <desmond.ng.yang.yi@gmail.com>
2015-01-11 21:40:42 +0800

    Add a new line from the text in the input of comment-input when the button is clicked

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -2,7 +2,14 @@ var main = function() {
 	"use-strict";
 
 	$(".comment-input button").on("click", function(event) {
-		$(".comments").append("<p>This is a new comment</p>");
+		// Add a $ to the var name if it holds a jQuery object
+		// jQuery allows for chaining function calls
+		var $new_comment = $("<p>");
+		var comment_text = $(".comment-input input").val();
+
+		$new_comment.text(comment_text);
+
+		$(".comments").append($new_comment);
 	});
 };
 
