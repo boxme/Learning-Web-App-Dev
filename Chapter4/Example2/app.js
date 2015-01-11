a27fc10bd773de2b59de5179df59d5f37192df9a
boxme <desmond.ng.yang.yi@gmail.com>
2015-01-11 20:37:55 +0800

    Attach an event listener to the button in Example2, to append the line 'this is a new comment' after capturing the onClick event

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,7 +1,9 @@
 var main = function() {
 	"use-strict";
 
-	window.alert("Hello World!");
+	$(".comment-input button").on("click", function(event) {
+		$(".comments").append("<p>This is a new comment</p>");
+	});
 };
 
 $(document).ready(main);
