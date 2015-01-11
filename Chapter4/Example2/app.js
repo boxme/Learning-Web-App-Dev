dc29a91f11311b953619f2f3e96816812688db97
boxme <desmond.ng.yang.yi@gmail.com>
2015-01-11 22:05:13 +0800

    Add a new comment with the text from the textbox when the 'Enter' keyboard button is pressed Fade in the new comment

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -10,7 +10,26 @@ var main = function() {
 			// jQuery allows for chaining function calls
 			var $new_comment = $("<p>");
 			$new_comment.text(comment_text);
+
+			// Hide the new comment first
+			$new_comment.hide();
+
 			$(".comments").append($new_comment);
+
+			// Fade in the new comment after the append
+			$new_comment.fadeIn();
+
+			// The val method of the jQuery object with an explicit val
+			// it will be filled with that value
+			$(".comment-input input").val("");
+		}
+	});
+
+	$(".comment-input input").on("keypress", function(event) {
+		// 'Enter' key is pressed
+		if (event.keyCode === 13) {
+			// Click the button programmatically
+			$(".comment-input button").trigger("click");
 		}
 	});
 };
