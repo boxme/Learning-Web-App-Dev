c64b79cdc6ce9f8a83debf34b87498f5a848f9eb
boxme <desmond.ng.yang.yi@gmail.com>
2015-02-02 00:06:46 +0800

    Flickr app allows the user to search for photos, but it's still displaying photos from the previous search

--- Chapter5/Flickr/javascripts/app.js
+++ Chapter5/Flickr/javascripts/app.js
@@ -1,27 +1,67 @@
-var main = function() {
+var main = function () {
 	"use strict";
+
+	var searchTerm = "singapore";
 
 	// This is actually just one string.
 	// but it's spread out over two lines for readablility
-	var url = "http://api.flickr.com/services/feeds/photos_public.gne" + 
-			  "?tags=singapore&format=json&jsoncallback=?";
+	var url = formSearchUrl(searchTerm);
 
-	$.getJSON(url, function(flickrResponse) {
-		flickrResponse.items.forEach(function (photo) {
-			// Create a new jQuery element to hold the image
-			// but hide it so we can fade it in
-			var $img = $("<img>").hide();
+	searchFlickrForPhotos(url);
 
-			// Set the attribute to the url
-			// contained in the response
-			$img.attr("src", photo.media.m);
+	var $searchField = $(".search-input input");
 
-			// Attach the img tag to the main photos element
-			$("main .photos").append($img);
-			$img.fadeIn();
-		});
+	$(".search-input button").on("click", function () {
+		searchTerm = $searchField.val();
+
+		if (searchTerm !== "") {
+			searchFlickrForPhotos(formSearchUrl(searchTerm));
+			$searchField.val("");
+		}
 	});
+};
 
+var searchFlickrForPhotos = function (url) {
+	$.getJSON(url, function (flickrResponse) {
+		displayFlickrPhotos(flickrResponse);
+	});
+};
+
+var displayFlickrPhotos = function (flickrResponse) {
+	var totalPhotosNum = flickrResponse.items.length;
+
+	var showPhoto = function (index) {
+		console.log(index);
+		var url = flickrResponse.items[index].media.m;
+
+		// Create a new jQuery element to hold the image
+		// but hide it so we can fade it in
+		var $img = $("<img>").hide();
+
+		// Set the attribute to the url
+		// contained in the response
+		$img.attr("src", url);
+
+		// Clear out the current photo
+		$("main .photos").empty();
+
+		// Attach the img tag to the main photos element
+		$("main .photos").append($img);
+
+		$img.fadeIn();
+
+		setTimeout(function () {
+			++index;
+			showPhoto(index % totalPhotosNum);
+		}, 3000);
+	};
+
+	showPhoto(0);
+};
+
+var formSearchUrl = function (searchTerm) {
+	return "http://api.flickr.com/services/feeds/photos_public.gne" + 
+		   "?tags=" + searchTerm + "&format=json&jsoncallback=?";
 };
 
 $(document).ready(main);
