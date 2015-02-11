e0a56fd95a96705ebf1096fb3cb8e3c4d3a9a267
boxme <desmond.ng.yang.yi@gmail.com>
2015-02-12 00:15:26 +0800

    Chapter 6 Amazeriffic gets its data from the server

--- Chapter6/VirtualMachine/app/Amazeriffic/client/javascripts/app.js
+++ Chapter6/VirtualMachine/app/Amazeriffic/client/javascripts/app.js
@@ -97,39 +97,10 @@ var main = function (toDoObjectss) {
 };
 
 $(document).ready(function () {
-	var toDoObjects = [
-		{
-			"description" : "Get groceries",
-			"tags" : ["shopping", "chores"]
-		},
-		{
-			"description" : "Make up some new ToDos",
-			"tags" : ["writing", "work"]
-		},
-		{
-			"description" : "Prep for Monday's class",
-			"tags" : ["work", "teaching"]
-		},
-		{
-			"description" : "Answer emails",
-			"tags" : ["work"]
-		},
-		{
-			"description" : "Take Gracie to the park",
-			"tags" : ["chores", "pets"]
-		},
-		{
-			"description" : "Finish writing this book",
-			"tags" : ["writing", "work"]
-		}
-	];
-
-	main(toDoObjects);
-	
-	// Need to be able to read local files from the webpage to do the below
-	// $.getJSON("todo.json", function (toDoObjects) {
-	// 	main(toDoObjects);
-	// });
+	$.getJSON("/todos.json", function (toDoObjects) {
+		console.log(JSON.stringify(toDoObjects));
+		main(toDoObjects);
+	});
 });
 
 var createToDoListFromObjects = function (toDoObjectss) {
